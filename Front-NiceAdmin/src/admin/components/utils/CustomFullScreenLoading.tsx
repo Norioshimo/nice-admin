8aa023183b39325b0cf0de3807c1ed8bae8b6b47
Norio <42630783+Norioshimo@users.noreply.github.com>
2026-01-17 09:38:16 -0300

    Mejorar el UX de loading full screen

--- Front-NiceAdmin/src/admin/components/utils/CustomFullScreenLoading.tsx
+++ Front-NiceAdmin/src/admin/components/utils/CustomFullScreenLoading.tsx
@@ -1,7 +1,13 @@
 export const CustomFullScreenLoading = () => {
   return (
-    <>
-      <h1>Cargando Espere...</h1> 
-    </>
+    <div className="d-flex flex-column justify-content-center align-items-center vh-100 bg-light">
+      {/* Spinner de Bootstrap */}
+      <div className="spinner-border text-primary mb-3" role="status">
+        <span className="visually-hidden">Cargando...</span>
+      </div>
+
+      {/* Mensaje */}
+      <h1>Cargando, por favor espere...</h1>
+    </div>
   );
 };
